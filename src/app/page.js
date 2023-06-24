4c26b396e7ffe7c521e342247da3ae713dd8e7bb
faizaledrus <faizal.edrus@gmail.com>
2023-06-24 09:52:11 +0700

    test window location

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ import React, {  useEffect } from 'react'
 export default function Home() {
 	const id = useConfig((state) => state.id)
 	const updateId=useConfig((state)=>state.updateId)
-	
+
 	const host = window.location.host
 	const subdomain = host.split('.')
 	const newDomain = `${subdomain[0]}.${subdomain[1]==='localhost:3000'?'deoapp.site':subdomain[1]}`
@@ -21,7 +21,7 @@ export default function Home() {
 	  getCollectionFirebase('domains',conditions)
 	  .then((x)=>{
 		console.log(x)  
-		updateId(x[0].id)})
+		updateId(x[0]?.id)})
 	  return () => {
 		
 	  }
